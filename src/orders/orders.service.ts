426c7a1b0fc7d1f07eabd1892356bd5e24cd970a
zalesiu <zalewmat31@gmail.com>
2024-08-12 18:27:15 +0200

    feat: add create, delete, update order actions, add functionality in service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Order, db } from 'src/db';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Injectable()
 export class OrdersService {
@@ -9,5 +12,27 @@ export class OrdersService {
 
   public getById(id: Order['id']): Order | null {
     return db.orders.find((o) => o.id === id);
+  }
+
+  public create(orderData: CreateOrderDto): Order {
+    const order = { ...orderData, id: uuidv4() };
+    db.orders.push(order);
+    return order;
+  }
+
+  public deleteById(id: Order['id']): void {
+    db.orders = db.orders.filter((order) => order.id !== id);
+  }
+
+  public updateById(id: Order['id'], orderData: UpdateOrderDto): void {
+    db.orders = db.orders.map((order) => {
+      if (order.id === id) {
+        return {
+          ...order,
+          ...orderData,
+        };
+      }
+      return order;
+    });
   }
 }
